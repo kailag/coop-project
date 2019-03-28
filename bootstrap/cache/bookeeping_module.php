8c8753cfcdce972a898421dc19172f28c1ba68c6
kailag <47238818+kailag@users.noreply.github.com>
2019-03-28 10:48:16 +0800

    Add files via upload

---
+++ bootstrap/cache/bookeeping_module.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'providers' => 
+  array (
+    0 => 'Modules\\Bookeeping\\Providers\\BookeepingServiceProvider',
+  ),
+  'eager' => 
+  array (
+    0 => 'Modules\\Bookeeping\\Providers\\BookeepingServiceProvider',
+  ),
+  'deferred' => 
+  array (
+  ),
+);
---
+++ bootstrap/cache/cashier_module.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'providers' => 
+  array (
+    0 => 'Modules\\Cashier\\Providers\\CashierServiceProvider',
+  ),
+  'eager' => 
+  array (
+    0 => 'Modules\\Cashier\\Providers\\CashierServiceProvider',
+  ),
+  'deferred' => 
+  array (
+  ),
+);
